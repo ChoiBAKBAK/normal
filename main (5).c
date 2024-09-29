033bd3282108f5ae1f69d2682d215f0c2c56e2ec
ChoiBAKBAK <cyat8538@naver.com>
2024-09-29 21:50:41 +0900

    Add files via upload

---
+++ main (5).c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+   
+   char message2[5]={'a','b','c','d','\0'};
+   char message[5]={'a','b','c','\0'};
+  
+   
+   printf("message= %s, message2= %s",message,message2);
+   return 0;
+}

8de9fb108da77f44e52d5b97d6ca053148662f41
ChoiBAKBAK <cyat8538@naver.com>
2024-11-24 13:33:15 +0900

    Add files via upload

---
+++ calcu.h
@@ -0,0 +1,19 @@
+#ifndef _CALCU_H
+#define _CALCU_H
+
+int add(int a,int b){
+    return a+b;
+}
+int sub(int a,int b){
+    return a-b;
+}
+int multi(int a,int b){
+    return a*b;
+}
+int printAdd(int a,int b){
+    int sum;
+    sum=0;
+    for(int i=a;i<b;i++)sum+=i;
+    return sum;
+} 
+#endif
